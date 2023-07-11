e37a35e31d2d80927092b8e87100624810cad15f
xero <x@xero.style>
2023-07-10 22:20:56 -0400

    style(indenting): гєŦ๏г๓คՇ Շђє קɭคภєՇ  💾

--- sofancy.js
+++ sofancy.js
@@ -2,13 +2,13 @@
 const fs = require("fs").promises;
 const path = require("path");
 let charmap,
-  find,
-  font,
-  str = "",
-  titles = false;
+	find,
+	font,
+	str = "",
+	titles = false;
 
 function usage() {
-  console.log(`usage: sofancy [-f (font) | -t] string
+	console.log(`usage: sofancy [-f (font) | -t] string
 
  flags:
   -f|--font (font) : output in a single font
@@ -22,71 +22,71 @@ txt: ᒪᒪᗩᗰᗩᔕ
   sofancy -t some string | fzf | xsel -i
   git commit -m "style(docs): $(sofancy -f bolditalic STYLIN)"
 `);
-  process.exit();
+	process.exit();
 }
 function reverse(s) {
-  return (s === '') ? '' : reverse(s.substr(1)) + s.charAt(0);
+	return (s === '') ? '' : reverse(s.substr(1)) + s.charAt(0);
 }
 function setArgs() {
-  const { parseArgs } = require("node:util");
-  const args = process.argv.slice(2);
-  const options = {
-    font: {
-      type: "string",
-      short: "f",
-    },
-    titles: {
-      type: "boolean",
-      short: "t",
-    },
-  };
-  try {
-    const { values, positionals } = parseArgs({
-      args,
-      options,
-      allowPositionals: true,
-    });
-    font = values.font || false;
-    find = !font ? /.*json$/ : new RegExp(`\\b${font}.json\\b`, "gi");
-    titles = values.titles || false;
-    str = (font == "upsidedown" || font == "mirrorflip")
-					? reverse(positionals.join(" "))
-					: positionals.join(" ") || usage();
-  } catch (e) {
-    usage();
-  }
+	const { parseArgs } = require("node:util");
+	const args = process.argv.slice(2);
+	const options = {
+		font: {
+			type: "string",
+			short: "f",
+		},
+		titles: {
+			type: "boolean",
+			short: "t",
+		},
+	};
+	try {
+		const { values, positionals } = parseArgs({
+			args,
+			options,
+			allowPositionals: true,
+		});
+		font = values.font || false;
+		find = !font ? /.*json$/ : new RegExp(`\\b${font}.json\\b`, "gi");
+		titles = values.titles || false;
+		str = (font == "upsidedown" || font == "mirrorflip")
+			? reverse(positionals.join(" "))
+			: positionals.join(" ") || usage();
+	} catch (e) {
+		usage();
+	}
 }
 const walk = async (dir, filelist = []) => {
-  const files = await fs.readdir(dir);
+	const files = await fs.readdir(dir);
 
-  for (const file of files) {
-    const filepath = path.join(dir, file);
-    const stat = await fs.stat(filepath);
+	for (const file of files) {
+		const filepath = path.join(dir, file);
+		const stat = await fs.stat(filepath);
 
-    if (stat.isDirectory()) {
-      filelist = await walk(filepath, filelist);
-    } else {
-      if (file.match(find)) {
-        filelist.push(file);
-        charmap = require(__dirname + `/fonts/${file}`);
-        let title = titles ? file.replace(".json", "").padEnd(15) : "";
-        console.log(
-          title +
-            str.replace(/./g, function (s) {
-              if (s in charmap) {
-                return charmap[s];
-              } else if (s.toLowerCase() in charmap) {
-                return charmap[s.toLowerCase()];
-              } else if (s.toUpperCase() in charmap) {
-                return charmap[s.toUpperCase()];
-              } else {
-                return s;
-              }
-            }) + " "
-        );
-      }
-    }
-  }
+		if (stat.isDirectory()) {
+			filelist = await walk(filepath, filelist);
+		} else {
+			if (file.match(find)) {
+				filelist.push(file);
+				charmap = require(__dirname + `/fonts/${file}`);
+				let title = titles ? file.replace(".json", "").padEnd(15) : "";
+				console.log(
+					title +
+					str.replace(/./g, function(s) {
+						if (s in charmap) {
+							return charmap[s];
+						} else if (s.toLowerCase() in charmap) {
+							return charmap[s.toLowerCase()];
+						} else if (s.toUpperCase() in charmap) {
+							return charmap[s.toUpperCase()];
+						} else {
+							return s;
+						}
+					}) + " "
+				);
+			}
+		}
+	}
 };
 setArgs();
 walk(__dirname + "/fonts");
